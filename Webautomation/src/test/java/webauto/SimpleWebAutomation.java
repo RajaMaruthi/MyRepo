6d4527727632a53ba1ad86ad6ee231a71f0a437c
RajaMaruthi <rekhaadbn@gmail.com>
2023-09-17 10:08:48 +0530

    Initial Commit

---
+++ Webautomation/src/test/java/webauto/SimpleWebAutomation.java
@@ -0,0 +1,56 @@
+package webauto;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SimpleWebAutomation
+{
+
+	WebDriver driver=null;
+	
+
+	@BeforeTest
+	void luanchBrowser()
+	{
+		
+		String strdriverpath=System.getProperty("user.dir")+"//src//test//resources//Drivers//chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", strdriverpath);
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
+		
+		
+	}
+	
+	
+	@Test()
+	public void login()
+	{
+		
+		driver.findElement(By.name("firstname")).sendKeys("Maruthiraja");
+		driver.findElement(By.name("lastname")).sendKeys("BN");
+		driver.findElement(By.xpath("//div/input[1][@name='sex']")).click();
+		
+	}
+	
+	@Test(priority = 1)
+	public void contanctUs()
+	{
+		
+		driver.findElement(By.xpath("//a[text()='Contact']")).click();
+		
+	}
+	
+	
+	@AfterTest
+	void tearDown()
+	{
+		driver.close();
+		driver.quit();
+	}
+
+}
